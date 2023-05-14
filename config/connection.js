d2bc6ce9fa091344ebfd8f5fd46d336a39283231
Leyla Callahan <leyla.callahan@yahoo.com>
2023-05-14 08:01:14 -0400

    finalconfig api

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(
-  // process.env.JAWSDB_URL,
   process.env.DB_NAME, 
   process.env.DB_USER, 
   process.env.DB_PASSWORD, 
